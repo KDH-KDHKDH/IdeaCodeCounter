eae07a7851ac92fa2f463e21d561ef2c7c576665
kkk <zyyxhmath@qq.com>
2024-10-22 21:10:15 +0800

    3 Apptable

---
+++ src/main/java/codecounter/counderapp/AppConstant.java
@@ -0,0 +1,12 @@
+package codecounter.counderapp;
+
+public class AppConstant {
+    static String csvFilePath = "output/output.csv";
+
+    /**
+     * 列名
+     */
+    static String[] initHeader = {"Extension", "File Count", "Total Lines", "Empty Lines", "Single Comment Lines", "Multi Comment Lines", "Code Lines"};
+
+
+}
